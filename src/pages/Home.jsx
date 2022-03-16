3711624561d9fa8f62fe23755c59fe26d83246ee
Rodrigo <rodrigomendozar10@gmail.com>
2022-03-16 16:50:01 -0300

    correcion de layout margin.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import CoverFigure from '../components/coverFigure/CoverFigure';
 function Home() {
 
     return (
-        <Container className=" d-flex justify-content-center aling-items-center my-5">        
+        <Container className=" d-flex justify-content-center aling-items-center">        
              <CoverFigure />
         </Container>
     )
