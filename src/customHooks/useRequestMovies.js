8604ae3cc21f6c56ee8532e7c0a7fc74b7fca90b
Rodrigo <rodrigomendozar10@gmail.com>
2022-03-22 11:30:21 -0300

    parameter en useEffect useRequestMovieById

--- src/customHooks/useRequestMovies.js
+++ src/customHooks/useRequestMovies.js
@@ -2,42 +2,41 @@ import { useEffect, useState } from "react";
 import { getMovie, getMovies } from "../services/moviesService";
 
 export function useRequestMovies(movieParam) {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-      (async function () {
-          setIsLoading(true);
-          const {error, data} = await getMovies(movieParam);
-          if ( data ) {
-              setData(data);
-          } else {
-              alert(error, 'erorr en la api');
-          }
-          setIsLoading(false)
-      })();
-    }, [movieParam]);
-    
+  useEffect(() => {
+    (async function () {
+      setIsLoading(true);
+      const { error, data } = await getMovies(movieParam);
+      if (data) {
+        setData(data);
+      } else {
+        alert(error, "erorr en la api");
+      }
+      setIsLoading(false);
+    })();
+  }, [movieParam]);
+
   return [data, isLoading];
 }
 
-
 export function useRequestMovieById(id) {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-      (async function () {
-          setIsLoading(true);
-          const {error, data} = await getMovie(id);
-          if ( data ) {
-              setData(data);
-          } else {
-              alert(error, 'erorr en la api');
-          }
-          setIsLoading(false)
-      })();
-    }, []);
-    
+  useEffect(() => {
+    (async function () {
+      setIsLoading(true);
+      const { error, data } = await getMovie(id);
+      if (data) {
+        setData(data);
+      } else {
+        alert(error, "erorr en la api");
+      }
+      setIsLoading(false);
+    })();
+  }, [id]);
+
   return [data, isLoading];
 }
